97be5bd8a284cb248934ad6c659baa203b83cb8e
Omurbek Dulatov <oma.dulatov@gmail.com>
2019-05-10 07:47:57 +0000

    added validation for input video url

--- apps/video_converter/forms.py
+++ apps/video_converter/forms.py
@@ -4,8 +4,10 @@ from .models import QueryHistory
 
 
 class DownloadForm(forms.ModelForm):
-    url = forms.CharField(widget=forms.TextInput(
-        attrs={'placeholder': 'вставьте ссылку для скачивания'}),
+    url = forms.RegexField(
+        regex=r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$',
+        widget=forms.TextInput(attrs={
+            'placeholder': 'вставьте ссылку для скачивания'}),
         label=False, required=True, max_length=500)
 
     class Meta:
--- apps/video_converter/views.py
+++ apps/video_converter/views.py
@@ -10,23 +10,23 @@ def index(request):
     if request.method == 'POST':
         form = DownloadForm(request.POST)
         if form.is_valid():
-            url = form.cleaned_data.get('url')
-            options = {
-                'outtmpl': '%(title)s-%(id)s.%(ext)s',
-                'format': 'best'
-            }
+                url = form.cleaned_data.get('url')
+                options = {
+                    'outtmpl': '%(title)s-%(id)s.%(ext)s',
+                    'format': 'best'
+                }
+                with youtube_dl.YoutubeDL(options) as ydl:
+                    r = ydl.extract_info(url, download=False)
+                    video_url = r['url']
+                    print(video_url)
+                    form.save()
+                    file_name = 'video.mp4'
+                    r = HttpResponsePermanentRedirect(video_url)
+                    r['Content-Type'] = 'application/force-download'
+                    r['Content-Disposition'] = f'attachment; filename={file_name}'
 
-            with youtube_dl.YoutubeDL(options) as ydl:
-                r = ydl.extract_info(url, download=False)
-                video_url = r['url']
-                print(video_url)
-                form.save()
-                file_name = 'video.mp4'
-                r = HttpResponsePermanentRedirect(video_url)
-                r['Content-Type'] = 'application/force-download'
-                r['Content-Disposition'] = f'attachment; filename={file_name}'
+                    return r
 
-                return r
         return render(request, 'video_converter/index.html', {'form': form})
     form = DownloadForm()
     return render(request, 'video_converter/index.html', {'form': form})
